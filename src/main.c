ccd4b20c7a40d4fd944609190263d8c1447248fb
srtGURU <yaremkoyurko@gmail.com>
2026-07-10 21:46:41 +0300

    Networl

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <sys/socket.h>   // socket(), bind(), listen(), accept()
+#include <netinet/in.h>   // sockaddr_in, htons(), htonl(), INADDR_ANY
+#include <arpa/inet.h>    // inet_ntoa(), inet_pton() тощо
+#include <unistd.h>       // close(), read(), write()
+#include <string.h>        // якщо використовуєш strlen(), memset() тощо
 
 int main()
 {
-    printf("Hello, world!\n");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;   // слухати на всіх інтерфейсах
+    addr.sin_port = htons(7777);          // ОБОВ'ЯЗКОВО htons для порту
+    
+    int opt = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+    
+    listen(sockfd, 5);
+
     return 0;
 }
